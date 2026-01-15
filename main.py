1dca071de09a21f9e70acdff73ec5b9fb04d17ec
Moshe <moshe@example.com>
2026-01-15 10:46:57 +0200

    feat: number of record and filtering information by schema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile
 import pandas as pd
 import uvicorn
+from models import InfoModel
 from pandas import DataFrame
 
 app = FastAPI()
@@ -9,13 +10,21 @@ app = FastAPI()
 @app.post('/top-threats')
 def get_data(file_csv: UploadFile):
     df = pd.read_csv(file_csv.file)
-    res = sort_danger(df)
-    print(res)
-    return {"massege":res.to_dict(orient='records')}
+    res = sort_by_danger_rate(df)
+    number_of_terrorest, spec_info = get_info(res)
+    print(spec_info)
+    return {
+        "count":number_of_terrorest,
+        "top":spec_info
+        }
 
-def sort_danger(data):
+def sort_by_danger_rate(data:DataFrame):
     sort = data.sort_values(by='danger_rate',ascending=False)
-    return sort.head(5)
+    return sort.head(5).to_dict(orient='records')
+
+
+def get_info(data:list[dict]):    
+    return len(data), [InfoModel(**record) for record in data]
 
 
 if __name__=="__main__":
