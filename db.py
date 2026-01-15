c38f4f67ed479db7f7d3a1b0b7552dbb936e8132
Moshe <moshe@example.com>
2026-01-15 14:39:17 +0200

    fix

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import bson
 
 MONGO_HOST = os.getenv("MONGO_HOST","127.0.0.1")
 MONGO_PORT = int(os.getenv("MONGO_PORT","27017"))
-MONGO_USERNAME = os.getenv("MONGO_USERMANE","mongodb")  
+MONGO_USERNAME = os.getenv("MONGO_USERMANE","admin")  
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD","secretpass")
 MONGO_DB = os.getenv("MONGO_DB","threat_db")
 MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE","admin")
@@ -12,9 +12,9 @@ MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE","admin")
 class MongoManager():
     def __init__(self):
         # for checking
-        url = "mongodb://127.0.0.1:27017" 
+        # url = "mongodb://127.0.0.1:27017" 
         uri = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
-        self.client = MongoClient(url)
+        self.client = MongoClient(uri)
         self.database = self.client[MONGO_DB]
         self.collection = self.database['top_threats']
 
