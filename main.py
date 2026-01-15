f7fa4f44af2a344f95ac9ac7e399428fb520b8a7
Moshe <moshe@example.com>
2026-01-16 00:40:30 +0200

    start fix

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile,HTTPException, Depends,File
 import pandas as pd
-import uvicorn
-from models import InfoModel
 from pandas import DataFrame
+from io import BytesIO
 import os
+import uvicorn
+from models import BasicInfoTerrorist, TopThreatsResponse
 from db import MongoManager
-app = FastAPI()
+
 
 MONGO_HOST = os.getenv("MONGO_HOST","mongo-0.mongo")
 MONGO_PORT = os.getenv("MONGO_PORT","27017")
-MONGO_USERNAME = os.getenv("MONGO_USERMANE","admin")  
+MONGO_USERNAME = os.getenv("MONGO_USERNAME","admin")  
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD","secretpass")
 MONGO_DB = os.getenv("MONGO_DB","threat_db")
 MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE","admin")
 
-# @app.on_event("/")
-# def initial_db():
-#     db = MongoManager()
 
-@app.post('/top-threats')
-def get_data(file_csv: UploadFile):
-    df = pd.read_csv(file_csv.file)
-    res = sort_by_danger_rate(df)
-    number_of_terrorest, spec_info = get_info(res)
-    
-    db = MongoManager()
+app = FastAPI()
+
+db = MongoManager()
+
+def get_db():
+    return db
+
+@app.post('/top-threats',response_model=TopThreatsResponse)
+async def getting_and_processing_data(file: UploadFile = File(...), database = Depends(get_db) ):
+    if not file.filename.endswith('.csv'):
+        raise HTTPException(status_code=400,detail="Invalid type file")
+    # 1. read from file
+    contatns = await file.read()
+    df = pd.read_csv(BytesIO(contatns))
+    # 2. Data processing
+    data = sort_by_danger_rate(df)
+    number_of_recoreds, spec_info = get_specific_info(data)
+    # 3. insert to db
     db.insert_all(spec_info)
+
     return {
-        "count":number_of_terrorest,
+        "count":number_of_recoreds,
         "top":spec_info
         }
 
 def sort_by_danger_rate(data:DataFrame):
-    sort = data.sort_values(by='danger_rate',ascending=False)
-    return sort.head(5).to_dict(orient='records')
+    """
+    sorting by column danger_rate 
+    return in dict form
+    
+    :param data: Description
+    :type data: DataFrame
+    return: data sorted in dict form
+    """
+    raw_records = data.sort_values(by='danger_rate',ascending=False).head(5)
+    return raw_records.to_dict(orient='records')
 
 
-def get_info(data:list[dict]):    
-    recoreds = [InfoModel(**record).model_dump() for record in data]
-    for i in range(len(recoreds)):
-        recoreds[i]['_id'] = i +1
-    return len(data), recoreds
+def get_specific_info(data: list[dict]) -> tuple[int,list[dict]]:    
+    recoreds = [BasicInfoTerrorist(**record).model_dump(by_alias=True) for record in data]
+    return len(recoreds), recoreds
     
 
 if __name__=="__main__":
--- models.py
+++ models.py
@@ -1,8 +1,23 @@
 from pydantic import BaseModel, Field
+from pymongo import MongoClient
+from typing import Annotated, Optional
+from pydantic import BeforeValidator, ConfigDict, Field,BaseModel
+
+PyObjectId = Annotated[str,BeforeValidator(str)]
 
 
-class InfoModel(BaseModel):
+class BasicInfoTerrorist(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str
     location:str
     danger_rate: int = Field(...,ge=1,le=10)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        extra='ignore'
+        )
+
+class TopThreatsResponse(BaseModel):
+    count: int
+    top: list[BasicInfoTerrorist]
 
