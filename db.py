f4950477801caefdce18525a48f8dc8ce7cc74a1
Moshe <moshe@example.com>
2026-01-18 10:46:01 +0200

    fix uri

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 import os
-import bson
+
 
 MONGO_HOST = os.getenv("MONGO_HOST","127.0.0.1")
 MONGO_PORT = int(os.getenv("MONGO_PORT","27017"))
-MONGO_USERNAME = os.getenv("MONGO_USERMANE","admin")  
+MONGO_USERNAME = os.getenv("MONGO_USERNAME","admin")  
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD","secretpass")
 MONGO_DB = os.getenv("MONGO_DB","threat_db")
 MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE","admin")
@@ -13,7 +13,7 @@ class MongoManager():
     def __init__(self):
         # for checking
         # url = "mongodb://127.0.0.1:27017" 
-        uri = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
+        uri = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/?authSource={MONGO_AUTH_SOURCE}"
         self.client = MongoClient(uri)
         self.database = self.client[MONGO_DB]
         self.collection = self.database['top_threats']
