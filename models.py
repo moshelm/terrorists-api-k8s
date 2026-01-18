a0a001cd859029a8a7a93ccf70d69020d02e762e
Moshe <moshe@example.com>
2026-01-18 10:50:33 +0200

    fix

--- models.py
+++ models.py
@@ -1,20 +1,19 @@
-from pydantic import BaseModel, Field
-from pymongo import MongoClient
+from pydantic import BaseModel, Field, BeforeValidator, ConfigDict
 from typing import Annotated, Optional
-from pydantic import BeforeValidator, ConfigDict, Field,BaseModel
 
-PyObjectId = Annotated[str,BeforeValidator(str)]
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class BasicInfoTerrorist(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name: str
-    location:str
-    danger_rate: int = Field(...,ge=1,le=10)
+    location: str
+    danger_rate: int = Field(..., ge= 1, le= 10)
     model_config = ConfigDict(
-        populate_by_name=True,
-        arbitrary_types_allowed=True,
-        extra='ignore'
+        populate_by_name = True,
+        arbitrary_types_allowed = True,
+        extra = 'ignore'
         )
 
 class TopThreatsResponse(BaseModel):
